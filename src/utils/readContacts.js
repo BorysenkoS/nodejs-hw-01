b01efcbc88d2ef1cbf127eae4de053568d496e37
Serhii Borysenko <borisenko37@gmail.com>
2024-09-30 13:24:26 +0200

    firts step is ok

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+};
+readContacts();
